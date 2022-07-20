a177cb006f71947ee3daecfed89210dffcb3603f
Aaroncul <aaronocullen@hotmail.co.uk>
2022-07-20 16:20:29 +0000

    Pending navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import ClaimForm from './Components/Forms/ClaimForm';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-        <ClaimForm formType="pet"/>
-    </div>
-  );
+  //return ()
+    //Create navbar with react-routing for pet/property/vehicle options
+  
 }
 
 export default App;
