32beabbfa04bc53b333c07fec1c86743c48d0cbd
Aaroncul <aaronocullen@hotmail.co.uk>
2022-07-27 15:27:47 +0000

    Replace navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import ClaimForm from './Components/Forms/ClaimForm';
 import React from 'react';
-import ClaimsNavBar from './Components/Menus/ClaimsNavBar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HeaderNavBar from './Components/Menus/HeaderNavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -12,8 +12,7 @@ function App() {
       <HeaderNavBar />
       
       <div className="content">
-        <Routes>
-        <Route exact path="new-claim" element={<ClaimsNavBar />} />
+        <Routes>        
           <Route exact path="new-claim/pet" element={<ClaimForm formType="pet"/>} />
           <Route exact path="new-claim/property" element={<ClaimForm formType="property"/>} />
           <Route exact path="new-claim/vehicle" element={<ClaimForm formType="vehicle"/>} />
