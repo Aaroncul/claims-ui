60ad8d055406567a8b1ab810be98d0a6b3db7089
Aaroncul <aaron.cullen@fernsoftware.com>
2022-07-28 12:06:17 +0100

    Bootstrapped nav bar. 404 and home screens (#1)
    * Replace navbar
    
    * Add a home and 404 component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import ClaimForm from './Components/Forms/ClaimForm';
 import React from 'react';
-import ClaimsNavBar from './Components/Menus/ClaimsNavBar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HeaderNavBar from './Components/Menus/HeaderNavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NotFound from './Components/NotFound';
+import Home from './Components/Home';
 
 function App() {
   return (
@@ -12,11 +14,13 @@ function App() {
       <HeaderNavBar />
       
       <div className="content">
-        <Routes>
-        <Route exact path="new-claim" element={<ClaimsNavBar />} />
+        <Routes>        
+        <Route exact path="home" element={<Home/>} />
           <Route exact path="new-claim/pet" element={<ClaimForm formType="pet"/>} />
           <Route exact path="new-claim/property" element={<ClaimForm formType="property"/>} />
           <Route exact path="new-claim/vehicle" element={<ClaimForm formType="vehicle"/>} />
+          <Route exact path="review-claim/" element={<ClaimForm formType="vehicle"/>} />
+          <Route exact path="*" element={<NotFound/>} />
         </Routes>
       </div>
     </div>
--- src/Components/Menus/HeaderNavBar.js
+++ src/Components/Menus/HeaderNavBar.js
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const HeaderNavBar = () => {
     return ( 
-        <nav className="NavBar">
-            <Link to="">Home</Link>
-            <span> | </span>
-            <Link to="new-claim">Make a claim</Link>
-            <span> | </span>
-            <Link to="login">Log in</Link>
-        </nav>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="home">AllState Claims</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown title="New Claim" id="basic-nav-dropdown">
+                            <NavDropdown.Item as={Link} to="new-claim/pet">Pet</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="new-claim/property">Property</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="new-claim/vehicle">Vehicle</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link as={Link} to="login">Log In</Nav.Link>                        
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>    
      );
 }
  
