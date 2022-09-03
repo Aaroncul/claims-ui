53e3f550f5c36b2e3ce54977ba872049b4a06b02
Aaroncul <aaronocullen@hotmail.co.uk>
2022-09-03 18:25:56 +0100

    Add initialClaim and data functions

--- src/Components/Forms/ClaimForm.js
+++ src/Components/Forms/ClaimForm.js
@@ -2,8 +2,39 @@ import PetSection from "./Claim form subtypes/PetSection";
 import PropertySection from "./Claim form subtypes/PropertySection";
 import VehicleSection from "./Claim form subtypes/VehicleSection";
 import LabelledInput from "./LabelledInputs/LabelledInput"
+import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import {addNewClaim} from '../../Data/DataFunctions'
+import {useReducer, useState} from "react";
 
 const ClaimForm = (props) => {
+
+    const initialNewClaimState = {
+        firstName : "", 
+        middleName : "",
+        lastName : "",
+        policyNumber : "",
+        claimDate : new Date().toISOString().slice(0, 10), 
+        amount : "0",
+        claimReason : "", 
+        incidentDescription : "USD", 
+        petAnimal : "", 
+        petBreed : "", 
+        propertyAddress : "",
+        vehicleMake:"",
+        vehicleModel:"",
+        vehicleYear:""};
+
+    const formReducer = (state, data) => {
+        return {
+            ...state,
+            [data.field]: data.value
+        };
+    }
+
+    const [newClaim, dispatch] = useReducer(formReducer , initialNewClaimState);
+
     console.log(props.formType)
     return (
         <div>
@@ -30,6 +61,25 @@ const ClaimForm = (props) => {
             }
         </div>
     )
+
+    const submit = (event) =>
+    {
+        event.preventDefault();
+        console.log("saving");
+        console.log(newClaim);
+        const response = addNewClaim(newClaim);
+        response.then(
+            result => {
+                if (result.status === 200) {
+                    console.log("new claim added!");
+                }
+                else {
+                    console.log("something went wrong - error code was " + result.status );
+                }
+            }
+        )
+        .catch(error => console.log("something went wrong ", error));
+    }
 }
 
 export default ClaimForm;
